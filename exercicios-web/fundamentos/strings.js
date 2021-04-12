efc0e2797fd57894f3da0b71b65b46a968978984
techmartins <martinsp.tech@outlook.com>
2021-04-12 08:06:35 -0300

    Estudando Strings.js

--- exercicios-web/fundamentos/strings.js
+++ exercicios-web/fundamentos/strings.js
@@ -2,7 +2,7 @@ const escola = "Cod3r"
 
 console.log(escola.charAt(4))
 console.log(escola.charAt(5))
-console.log(escola.charCodeAt(3))
+console.log(escola.charCodeAt(3)) //retorna o código relacionado a tabela unicode
 console.log(escola.indexOf('3'))
 
 console.log(escola.substring(1))
